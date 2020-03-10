603a6eadefc0160e3899925f0b9f4d5a337c724c
Peter Molnar <peter.molnar@falcon.io>
2020-03-10 12:53:59 +0100

    feat: inital commit

---
+++ src/main/java/io/falcon/demo/petersjavaapp/PetersJavaAppApplication.java
@@ -0,0 +1,13 @@
+package io.falcon.demo.petersjavaapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetersJavaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetersJavaAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/falcon/demo/petersjavaapp/PetersJavaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package io.falcon.demo.petersjavaapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetersJavaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
